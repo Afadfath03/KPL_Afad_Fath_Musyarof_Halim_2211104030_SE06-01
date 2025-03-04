1dca13e589997cbab42aed855cda0e7ea987718c
Afad Fath Musyarof Halim <afadfath03@gmail.com>
2025-03-04 22:51:24 +0700

    tp

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_CSharp/tpmodul2_2211104030/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Masukkan satu huruf: ");
+string huruf = Console.ReadLine().ToLower();
+
+if(huruf == "a" | huruf == "i" | huruf == "u" | huruf == "e" | huruf == "o") {
+    Console.WriteLine("Huruf " + huruf + " adalah huruf vokal");
+} else {
+    Console.WriteLine("Huruf " + huruf + " adalah huruf konsonan");
+}
+
+//Console.WriteLine();
+//Console.WriteLine();
+//Console.WriteLine();
+//Console.WriteLine();
+
+//int[] genap = { 2, 4, 6, 8, 10 };
+
+//for(int i = 0; i < genap.Length; i++) {
+//    Console.WriteLine("Angka Genap " + (i+1) + ": " + genap[i]);
+//}
