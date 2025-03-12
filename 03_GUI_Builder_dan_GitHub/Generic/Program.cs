bf94c8fd533f1334993159a89a949d8060a9025d
Afad Fath Musyarof Halim <afadfath03@gmail.com>
2025-03-12 22:02:17 +0700

    TP Modul 5

---
+++ 03_GUI_Builder_dan_GitHub/Generic/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace generic {
+    class Program {
+        static void Main(string[] args) {
+            int[] arr = { 0, 1, 2, 3, 4 };
+
+            List<int> list = new List<int>();
+            for(int x = 5; x < 10; x++) {
+                list.Add(x);
+            }
+            ProcessItem(arr);
+            ProcessItem(list);
+        }
+
+        static void ProcessItem<T>(IList<T> Cell) {
+            // IsReadOnly return true for the array and false for the list
+            System.Console.WriteLine("IsReadOnly");
+
+            // The following statement cause a run-time exception for the array but not for the list
+            // cell.removeat(4)
+            foreach(T item in Cell) {
+                System.Console.Write(item.ToString() + "");
+            }
+            System.Console.WriteLine();
+        }
+    } 
+}
---
+++ 03_GUI_Builder_dan_GitHub/Modul 5/tpmodul5_2211104030/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program {
+    private static void Main(string[] args) {
+        Console.Write("Masukkan nama: ");
+        string nama = Console.ReadLine();
+        HaloGeneric.SapaUser(nama);
+    }
+}
+
+class HaloGeneric {
+    public static void SapaUser(string name) {
+        Console.WriteLine($"Halo user {name}");
+    }
+}
